ce25854727ae98818fd295586dbde21abf8618a0
sania924 <saniayasin924@gmail.com>
2025-01-02 21:43:29 +0500

    update

---
+++ EMPLOY-APP/src/app/pages/employeeproject/employeeproject.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeeproject',
+  imports: [],
+  templateUrl: './employeeproject.component.html',
+  styleUrl: './employeeproject.component.scss'
+})
+export class EmployeeprojectComponent {
+
+}
